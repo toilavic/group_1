cad34e9d3a8ceabfdae10f0feefa5aaac2104e85
ngocdu <ngocdu171@gmail.com>
2021-11-09 00:26:56 +0200

    create StoresContext file to manage all state of app

---
+++ frontend/src/contexts/StoresContext.tsx
@@ -0,0 +1,37 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import interfaceStore from "../data/stores/interfaceStore";
+import store from "../data/stores/stores";
+
+interface StoreContextProps{
+    children: ReactNode
+}
+
+export interface StoresContextDefault{
+    stores: interfaceStore[],
+}
+
+const storesContextDataDefault = {
+    stores: []
+}
+
+export const StoresContext = createContext<StoresContextDefault>(
+    storesContextDataDefault
+);
+
+const StoresContextProvider = ({children} : StoreContextProps) => {
+    const [stores, setStores] = useState<interfaceStore[]>(storesContextDataDefault.stores);
+
+    useEffect(() => {
+        setStores(store)
+    }, [])
+
+    const StoresContextData = {
+        stores,
+    }
+
+    return (
+        <StoresContext.Provider value={ StoresContextData }>
+            {children}
+        </StoresContext.Provider>
+    )
+}

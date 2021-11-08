1238f0b5b415d1484fd80c83afce1ee3bf1b31f6
ngocdu <ngocdu171@gmail.com>
2021-11-09 00:43:04 +0200

    import StoresContextProvider to App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,19 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import StoresContextProvider from "./contexts/StoresContext";
 import "./global.css";
 import HomePage from "./pages/homepage/HomePage";
 import MapPage from "./pages/map/MapPage";
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/map" component={MapPage} />
-      </Switch>
-    </BrowserRouter>
+    <StoresContextProvider>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/map" component={MapPage} />
+        </Switch>
+      </BrowserRouter>
+    </StoresContextProvider>
   );
 };
 

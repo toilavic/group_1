ccce9759eb238739c7e332cce609ed8f08b47637
ngocdu <ngocdu171@gmail.com>
2021-11-08 23:37:43 +0200

    define type of store

---
+++ frontend/src/data/stores/interfaceStore.ts
@@ -0,0 +1,16 @@
+export default interface interfaceStore {
+    id: number,
+    name: string,
+    address: string,
+    rate: [],
+    price: number,
+    openTime: string,
+    contactNumber: string,
+    description: string,
+    discountRate: number,
+    instagram: string,
+    createdAt: string,
+    updateAt: string,
+    lat: number,
+    long: number
+}

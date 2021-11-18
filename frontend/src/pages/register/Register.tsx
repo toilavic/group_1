2e4ed78ed3be28eeb2c353ddf83dff4a50b84bcb
ngocdu <ngocdu171@gmail.com>
2021-11-18 22:27:16 +0200

    pass value to StoresContext

--- frontend/src/pages/register/Register.tsx
+++ frontend/src/pages/register/Register.tsx
@@ -8,18 +8,21 @@ import {
 } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import { ChangeEvent, FC, useState } from "react";
+import { ChangeEvent, FC, useContext, useState } from "react";
 import { Link } from "react-router-dom";
+import { StoresContext } from "../../contexts/StoresContext";
 
 const theme = createTheme();
 
 const Register: FC = () => {
+  const {Register} = useContext(StoresContext);
 
-    const [email, setEmail] = useState<string>();
-    const [password, setPassword] = useState<string>();
+    const [username, setUsername] = useState<string>("")
+    const [name, setName] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
 
     const handleRegister = (event:ChangeEvent<HTMLInputElement>) => {
-        console.log("Email: ",email,"/n","Password: ", password );
+        Register(username, name, password);
         event.preventDefault();
     };
 
@@ -40,37 +43,28 @@ const Register: FC = () => {
           </Typography>
           <Box component="form" onSubmit={handleRegister} sx={{ mt: 3 }}>
             <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
+            <Grid item xs={12}>
                 <TextField
                   required
                   fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
+                  id="username"
+                  label="Username"
+                  name="username"
+                  autoComplete="username"
+                  value={username}
+                  onChange={(event) => setUsername(event.target.value)}
                 />
               </Grid>
               <Grid item xs={12}>
                 <TextField
                   required
                   fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                  value={email}
-                  onChange={(event) => setEmail(event.target.value)}
+                  id="name"
+                  label="Name"
+                  name="name"
+                  autoComplete="name"
+                  value={name}
+                  onChange={(event) => setName(event.target.value)}
                 />
               </Grid>
               <Grid item xs={12}>

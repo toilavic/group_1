545293b194c488ef1543d5023e214752ff771883
Thinh Duc <t9ngdu00@students.oamk.fi>
2021-11-22 18:38:52 +0200

    Setting up User Authentication

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -19,17 +19,26 @@ router.post('/register', async (req, res) => {
     //Hash passwords
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(req.body.passwordHash, salt);
-    //Create a new user
-    const user = new User({
-        username: req.body.username,
-        name: req.body.name,
-        passwordHash: hashedPassword
-    });
-    try{
-        const savedUser = await user.save();
-        res.send({user: user._id, name: savedUser.name});
 
-    }catch(err){
+    try {
+        const {role} = req.body;
+        //Create a new user
+        const user = new User({
+            username: req.body.username,
+            name: req.body.name,
+            passwordHash: hashedPassword,
+            role: role || 'admin'
+        });
+        const accessToken = jwt.sign({userId: user._id}, process.env.SECRET, {
+            expiresIn: '1d'
+        });
+        user.accessToken = accessToken;
+        const savedUser = await user.save();
+        res.json({
+            user: savedUser,
+            message: "You have signed up successfully"
+        });
+}catch(err){
         res.status(404).send(err);
     }
 });
@@ -47,8 +56,33 @@ router.post('/login', async (req, res) => {
         if(!validPass) return res.status(400).send('Invalid password');
 
         //Create and assign a token 
-        const token = jwt.sign({_id: user._id}, process.env.SECRET)
-        res.header('auth-token', token).send(token);
+        const accessToken = jwt.sign({userId: user._id}, process.env.SECRET, {
+            expiresIn: '1d'
+        });
+        await User.findByIdAndUpdate(user._id, {accessToken})
+        res.status(200).json({
+            user: {username: user.username, role: user.role },
+            accessToken
+        });    
 });
 
+//GET ALL USERS
+router.get('/users', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (err) {
+        res.json({message: err});
+    }
+})
+
+//GET USER BY ID
+router.get('/users/:userId', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.userId);
+        res.json(user);
+    } catch (err) {
+        res.json({message: err});
+    }
+})
 module.exports = router;

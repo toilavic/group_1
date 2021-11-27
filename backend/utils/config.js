da75156c583524441a07b9ec9c78d26351d544e8
Thinh Duc <t9ngdu00@students.oamk.fi>
2021-11-27 08:22:23 +0200

    Mongodb database for the production environment

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -7,6 +7,10 @@ if (process.env.NODE_ENV === "test") {
   MONGODB_URI = process.env.TEST_MONGODB_URI;
 }
 
+if (process.env.NODE_ENV === "production") {
+  MONGODB_URI = process.env.MONGODB_URI;
+}
+
 module.exports = {
   PORT,
   MONGODB_URI,

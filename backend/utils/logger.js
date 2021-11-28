63b2bc6f7534562cc7276a03348f8f616a7bf4c6
Vic QT git <t18042k@gmail.com>
2021-11-28 15:46:50 +0200

    Added logger console for test env

--- backend/utils/logger.js
+++ backend/utils/logger.js
@@ -1,14 +1,14 @@
 const info = (...params) => {
-    if (process.env.NODE_ENV !== "production") {
-      console.log(...params);
-    }
-  };
-  
-  const error = (...params) => {
-    console.error(...params);
-  };
-  
-  module.exports = {
-    info,
-    error,
-  };
+  if (process.env.NODE_ENV !== "test") {
+    console.log(...params);
+  }
+};
+
+const error = (...params) => {
+  console.error(...params);
+};
+
+module.exports = {
+  info,
+  error,
+};

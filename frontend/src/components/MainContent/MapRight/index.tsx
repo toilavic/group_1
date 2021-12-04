b4618576aa785f8eb12bced8a85c1b7591438afd
Vic QT git <t18042k@gmail.com>
2021-12-04 18:01:04 +0200

    Add hover status to mapright

--- frontend/src/components/MainContent/MapRight/index.tsx
+++ frontend/src/components/MainContent/MapRight/index.tsx
@@ -3,13 +3,17 @@ import Showmap from "../../showmap/Showmap";
 import { useContext } from "react";
 import { StoresContext } from "../../../contexts/StoresContext";
 
-const MapRight = () => {
+interface Props {
+    selectStore: (id: string) => any;
+}
+
+const MapRight: React.FC<Props> = ({selectStore}) => {
 
     const { stores } = useContext(StoresContext);
 
     return (
         <div style = {{height: '90vh', width: '40vw', borderRadius: '10px', overflow: 'hidden' }}>
-            <Showmap stores={stores} />
+            <Showmap stores={stores} selectStore = {selectStore}/>
         </div>
 
     );

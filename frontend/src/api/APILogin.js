8578799f497b6017e927f717cf8e44e68ec39caa
ngocdu <ngocdu171@gmail.com>
2021-12-04 15:27:25 +0200

    pass value to APILogin

--- frontend/src/api/APILogin.js
+++ frontend/src/api/APILogin.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import constants from "../constants.json";
 
 export default function APILogin(username, passwordHash) {
+  console.log(username, passwordHash);
   return axios.post(constants.baseAddress + `auth/login`, { username, passwordHash })
     .then((response) => response.data)
     .catch((error) => console.log(error));

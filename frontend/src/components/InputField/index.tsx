528e060ef73c7e4f12af0d3f174985548b7d8e97
ngocdu <ngocdu171@gmail.com>
2021-12-04 15:28:54 +0200

    create InputField component

--- frontend/src/components/InputField/index.tsx
+++ frontend/src/components/InputField/index.tsx
@@ -4,16 +4,14 @@ import { useField } from "formik"
 interface InterfaceTextFieldProps {
     name: string
     label: string
-    placeholder: string
     type: string
 }
 
 const InputField = (props: InterfaceTextFieldProps) => {
     const [field, {error}] = useField(props)
     return (
-        <Grid item xs={12}>
-            <InputLabel htmlFor={field.name}>{props.label}</InputLabel>
-            <TextField {...field} id={field.name} {...props} />
+        <Grid item xs={12} marginLeft="20%" padding="10px 10px">
+            <TextField {...field} id={field.name} {...props} required style={{width:500}} />
             {error && <Typography variant="caption">{error}</Typography>}
         </Grid>
     )
